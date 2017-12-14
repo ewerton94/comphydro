ea863e090a3a53ee7367d8ecedde59eb5b75475d
Ewerton Oliveira <ewerton.amorim@live.com>
2017-12-14 15:56:39 -0300

    Atualizados formulários de séries de reamostragem

--- stats/forms.py
+++ stats/forms.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 from data.models import Discretization,OriginalSerie
 from stations.models import Station
+from .models import Distribution
  
     
 
@@ -49,3 +50,16 @@ class IHAForm(forms.Form):
                                 )
     initial = forms.CharField(label=_("Initial year")+":",widget=forms.TextInput(attrs={'class':'form-control'}))
     final = forms.CharField(label=_("Final year")+":",widget=forms.TextInput(attrs={'class':'form-control'}))
+
+
+class SerieFromParametersForm(forms.Form):
+    probability_or_aleatory = forms.ChoiceField(label=_("Probability Values or Aleatory?")+":",required=False,widget=forms.Select(attrs={'class':'form-control'}),choices=
+                                 ((1,_("Probabilities")),(2,_("Aleatory"))))
+    probability_values = forms.CharField(label=_("Values of probability (numbers separateds by commas)")+":",required=False,widget=forms.TextInput(attrs={'class':'form-control'}))
+    sample_size = forms.IntegerField(label=_("Size of sample")+":",required=False,widget=forms.TextInput(attrs={'class':'form-control'}))
+    distribution = forms.ChoiceField(label=_("Distribution")+":",widget=forms.Select(attrs={'class':'form-control'}),choices=
+                                 ((distribution.abreviation, distribution.name) for distribution in Distribution.objects.all()))
+    alpha = forms.FloatField(label=_("Apha")+":",widget=forms.TextInput(attrs={'class':'form-control'}))
+    betha = forms.FloatField(label=_("Betha")+":",widget=forms.TextInput(attrs={'class':'form-control'}))
+    kappa = forms.FloatField(label=_("Kappa")+":",widget=forms.TextInput(attrs={'class':'form-control'}))
+
